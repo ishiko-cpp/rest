d9dff931bc4f7d2b4b7ec82a38aee8c112fdc762
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-10-11 23:15:51 +0100

    Add files

---
+++ include/Ishiko/REST/RESTClient.hpp
@@ -0,0 +1,17 @@
+/*
+    Copyright (c) 2023 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/rest/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_HTTP_HTTPCLIENT_HPP_
+#define _ISHIKO_CPP_HTTP_HTTPCLIENT_HPP_
+
+namespace Ishiko
+{
+    class RESTClient
+    {
+    };
+}
+
+#endif
---
+++ src/RESTClient.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2023 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/rest/blob/main/LICENSE.txt
+*/
+
+#include "RESTClient.hpp"
